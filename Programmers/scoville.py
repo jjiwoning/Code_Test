6c137e344fa1fedac43dc4c9d228caa9f3ac85e6
JJiWoNing <jjiwoning12@gmail.com>
2022-03-09 02:53:44 +0900

    3/9_Study

---
+++ Programmers/scoville.py
@@ -0,0 +1,16 @@
+# 더 맵게
+
+import heapq
+
+def solution(scoville, K):
+    scoville.sort()
+    cnt = 0
+    while scoville[0] < K:
+        if len(scoville) <= 1:
+            return -1
+        a = heapq.heappop(scoville)
+        b = heapq.heappop(scoville)
+        new = a + 2 * b
+        heapq.heappush(scoville, new)
+        cnt += 1
+    return cnt
