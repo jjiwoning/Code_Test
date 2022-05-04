c7ed1de2aa3cbc4bf6f0c811b0175f59d6c8d51b
JJiWoNing <jjiwoning12@gmail.com>
2022-05-04 16:29:41 +0900

    5/4_study

--- Programmers/kakao_chartedit.py
+++ Programmers/kakao_chartedit.py
@@ -5,5 +5,52 @@
 # cmd : 수행한 명령어
 
 def solution(n, k, cmd):
-    answer = ''
-    return answer
+    answer = ['O'] * n
+    remember = []
+    for i in cmd:
+        if i[0] == 'U':
+            cnt = int(i[2:])
+            while cnt:
+                if answer[k-1] != 'X':
+                    cnt -= 1
+                    k -= 1
+                else:
+                    k -= 1
+        elif i[0] == 'D':
+            cnt = int(i[2:])
+            while cnt:
+                if answer[k+1] != 'X':
+                    cnt -= 1
+                    k += 1
+                else:
+                    k += 1
+        elif i == 'C':
+                answer[k] = 'X'
+                remember.append(k)
+                if k == n-1 or ('O' not in answer[k+1:]):
+                    cnt = 1
+                    while cnt:
+                        if answer[k-1] != 'X':
+                            cnt -= 1
+                            k -= 1
+                        else:
+                            k -= 1
+                else:
+                    cnt = 1
+                    while cnt:
+                        if answer[k+1] != 'X':
+                            cnt -= 1
+                            k += 1
+                        else:
+                            k += 1
+
+        else:
+            answer[remember.pop()] = 'O'
+
+    return ''.join(answer)
+
+n = 8
+k = 2
+cmd = ["D 2","C","U 3","C","D 4","C","U 2","Z","Z"]
+
+print(solution(n, k, cmd))
