98eccdd632c3b5bbf60b20e0dc46f9d5aeb655c8
JJiWoNing <jjiwoning12@gmail.com>
2022-05-06 18:13:47 +0900

    5/6_Study

--- test2.py
+++ test2.py
@@ -1,1 +1,23 @@
-print("Hello, GitHub!")
+from collections import deque
+
+def solution(priorities, location):
+    stk = deque()
+    prior = deque(priorities)
+    for i in range(len(priorities)):
+        stk.append(i)
+    cnt = 0
+    while prior:
+        maxPrior = max(prior)
+        if prior[0] != maxPrior:
+            prior.append(prior.popleft())
+            stk.append(stk.popleft())
+        else:
+            prior.popleft()
+            a = stk.popleft()
+            cnt += 1
+            if a == location:
+                return cnt
+
+#solution([2, 1, 3, 2], 2)
+
+
