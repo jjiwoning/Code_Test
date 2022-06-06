5eb8db5535a1f879ab5849d455cb11a54c921088
JJiWoNing <jjiwoning12@gmail.com>
2022-06-06 20:01:19 +0900

    자바 환경 설정

---
+++ test.java
@@ -0,0 +1,8 @@
+import javax.xml.transform.Source;
+
+class test{
+    public static void main(String[] args) {
+        System.out.println("tomtom");
+        System.out.println("Hello");
+    }
+}
