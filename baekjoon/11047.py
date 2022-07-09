26e3d6ff0396fdb5955e773b53fe255c34e5ff4c
JJiWoNing <jjiwoning12@gmail.com>
2022-07-09 23:44:13 +0900

    7/9_Study_Greedy

--- baekjoon/11047.py
+++ baekjoon/11047.py
@@ -2,19 +2,33 @@
 
 import sys
 
+def solution(money, k):
+    answer = 0
+    money.reverse()
+    for i in money:
+        answer += (k // i)
+        k = k % i
+    return answer
+
 n, k = map(int, sys.stdin.readline().split())
 moneyList = []
 
 for i in range(n):
     moneyList.append(int(sys.stdin.readline()))
 
-cnt = 0
-# 리스트 뒤집기 (내림차순)
-moneyList.reverse()
+money = moneyList
 
-# 몫 -> 카운트 , 나머지 -> 잔돈
-for i in moneyList:
-    cnt += k//i
-    k = k%i
+answer = solution(money, k)
+print(answer)
+
+# cnt = 0
+# # 리스트 뒤집기 (내림차순)
+# moneyList.reverse()
+
+# # 몫 -> 카운트 , 나머지 -> 잔돈
+# for i in moneyList:
+#     cnt += k//i
+#     k = k%i
     
-print(cnt)
+# print(cnt)
+
