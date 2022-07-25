b0b10b34a2849fd0b8cd9c820d302577ad2fd26c
JJiWoNing <jjiwoning12@gmail.com>
2022-07-25 14:06:43 +0900

    7/25 Study

--- baekjoon_java/P11720/Main.java
+++ baekjoon_java/P11720/Main.java
@@ -1,4 +1,22 @@
 package baekjoon_java.P11720;
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        String sNum = sc.next();
+        char[] cNum = sNum.toCharArray();
+        int sum = 0;
+
+        for (char c : cNum) {
+            sum += c - '0';
+        }
+
+        System.out.println(sum);
+    }
+
 }
