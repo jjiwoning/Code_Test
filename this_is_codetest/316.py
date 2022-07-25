4dee345543335325cf30c3ff2cdc8c745d4fb256
JJiWoNing <jjiwoning12@gmail.com>
2022-07-25 11:07:42 +0900

    7/25 Study

--- this_is_codetest/316.py
+++ this_is_codetest/316.py
@@ -9,18 +9,18 @@ def solution(food_time, k):
         # (음식 시간, 음식 번호) 형태로 큐에 넣는다.
         heapq.heappush(queue, (food_time[i], i + 1))
 
-        sum = 0 # 음식을 먹기 위해 사용한 시간 총 합
-        previous = 0 # 직전에 다 먹은 음식 시간
-        length = len(food_time) # 남은 음식 갯수
+    sum = 0 # 음식을 먹기 위해 사용한 시간 총 합
+    previous = 0 # 직전에 다 먹은 음식 시간
+    length = len(food_time) # 남은 음식 갯수
 
-        while sum + ((queue[0][0] - previous) * length) <= k:
-            now = heapq.heappop(queue)[0]
-            sum += (now - previous) * length
-            length -= 1 # 다 먹은 음식 제외 (큐에서 빠졌음)
-            previous = now # 이전 음식 시간 재설정
+    while sum + ((queue[0][0] - previous) * length) <= k:
+        now = heapq.heappop(queue)[0]
+        sum += (now - previous) * length
+        length -= 1 # 다 먹은 음식 제외 (큐에서 빠졌음)
+        previous = now # 이전 음식 시간 재설정
         
-        result = sorted(queue, key = lambda x : x[1])
-        return result[(k - sum) % length][1]
+    result = sorted(queue, key = lambda x : x[1])
+    return result[(k - sum) % length][1]
     
 
 
