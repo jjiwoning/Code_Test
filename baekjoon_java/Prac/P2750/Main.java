cede3d3e0c36ee692b6c557c5c833b74cb04dfc2
JJiWoNing <jjiwoning12@gmail.com>
2022-07-29 18:10:48 +0900

    7/29 Study

---
+++ baekjoon_java/Prac/P2750/Main.java
@@ -0,0 +1,30 @@
+package baekjoon_java.Prac.P2750;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < N - 1; i++) {
+            for (int j = 0; j < N - 1 - i; j++) {
+                if(arr[j] > arr[j + 1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+
+}
