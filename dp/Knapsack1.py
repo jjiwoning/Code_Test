3303adc6107ee4edf8f988dcc4dd7e99bc0f8567
JJiWoNing <jjiwoning12@gmail.com>
2022-10-08 22:17:43 +0900

    10/8_Study

--- dp/Knapsack1.py
+++ dp/Knapsack1.py
@@ -1,10 +1,14 @@
 import sys
 
+
 if __name__ == '__main__':
+
     n, m = map(int, sys.stdin.readline().split())
     dy = [0] * (m + 1)
+
     for i in range(n):
         w, v = map(int, sys.stdin.readline().split())
+        
         for j in range(w, m + 1):
             dy[j] = max(dy[j], dy[j - w] + v)
 
