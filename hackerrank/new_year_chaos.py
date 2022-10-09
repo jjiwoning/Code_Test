d474f9e3fbbf90e337744e288d52a9feccf7be2f
JJiWoNing <jjiwoning12@gmail.com>
2022-10-09 22:45:47 +0900

    10/9_Study

--- hackerrank/new_year_chaos.py
+++ hackerrank/new_year_chaos.py
@@ -5,12 +5,6 @@ import re
 import sys
 
 
-#
-# Complete the 'minimumBribes' function below.
-#
-# The function accepts INTEGER_ARRAY q as parameter.
-#
-
 def minimumBribes(q):
     # Write your code here
     for i in range(len(q)):
