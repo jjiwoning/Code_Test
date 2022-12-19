f19e5b4145aa854f0e61e3742b94648851c5c3bd
JJiWoNing <jjiwoning12@gmail.com>
2022-12-19 21:53:37 +0900

    12/19 Study

---
+++ java_algo/Programmers_java/three_musketeers/Solution.java
@@ -0,0 +1,20 @@
+package Programmers_java.three_musketeers;
+
+/**
+ * 프로그래머스 - 삼총사
+ */
+public class Solution {
+    public int solution(int[] number) {
+        int answer = 0;
+        for (int i = 0; i < number.length; i++) {
+            for (int j = i + 1; j < number.length; j++) {
+                for (int k = j + 1; k < number.length; k++) {
+                    if (number[i] + number[j] + number[k] == 0) {
+                        answer++;
+                    }
+                }
+            }
+        }
+        return answer;
+    }
+}
