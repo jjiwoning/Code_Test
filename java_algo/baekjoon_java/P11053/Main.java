e903b17004c83fbac19942b195240546b8eb7a95
JJiWoNing <jjiwoning12@gmail.com>
2023-01-10 20:51:15 +0900

    1/10 Study

---
+++ java_algo/baekjoon_java/P11053/Main.java
@@ -0,0 +1,31 @@
+package baekjoon_java.P11053;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+        int[] dp = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < n; i++) {
+            dp[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (arr[j] < arr[i] && dp[i] < dp[j] + 1) {
+                    dp[i] = dp[j] + 1;
+                }
+            }
+        }
+
+        int answer = Arrays.stream(dp).max().getAsInt();
+        System.out.println(answer);
+    }
+}
