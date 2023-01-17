e1443cbae7981643729aa10d28502498effee6db
JJiWoNing <jjiwoning12@gmail.com>
2023-01-17 20:31:35 +0900

    1/17 Study

---
+++ java_algo/baekjoon_java/P9251/Main.java
@@ -0,0 +1,29 @@
+package baekjoon_java.P9251;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        char[] chars1 = sc.next().toCharArray();
+        char[] chars2 = sc.next().toCharArray();
+
+        int char1Length = chars1.length;
+        int char2Length = chars2.length;
+
+        int[][] dp = new int[char1Length + 1][char2Length + 1];
+
+        for (int i = 1; i < char1Length; i++) {
+            for (int j = 1; j < char2Length; j++) {
+                if (chars1[i] == chars2[i]) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                    continue;
+                }
+                dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+
+        System.out.println(dp[char1Length][char2Length]);
+    }
+}
