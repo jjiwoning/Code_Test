1998e638432a574aa89fd8ac21f44b9f5898ccbf
JJiWoNing <jjiwoning12@gmail.com>
2023-03-01 18:21:47 +0900

    3/1 Study

---
+++ java_algo/baekjoon_java/P1271/Main.java
@@ -0,0 +1,15 @@
+package baekjoon_java.P1271;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        BigInteger n = sc.nextBigInteger();
+        BigInteger m = sc.nextBigInteger();
+
+        System.out.println(n.divide(m));
+        System.out.println(n.mod(m));
+    }
+}
