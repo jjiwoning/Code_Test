db29d9cb7364759c277af79c69084ee03441eb2c
JJiWoNing <jjiwoning12@gmail.com>
2023-05-27 19:34:35 +0900

    5/27 study

---
+++ java_algo/Programmers_java/P154539/Solution.java
@@ -0,0 +1,32 @@
+package Programmers_java.P154539;
+
+import java.util.Stack;
+
+public class Solution {
+
+    public int[] solution(int[] numbers) {
+        int[] answer = new int[numbers.length];
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < numbers.length; i++) {
+
+            if (stack.isEmpty() || numbers[i] < numbers[i - 1]) {
+                stack.push(i);
+                continue;
+            }
+
+            while (!stack.isEmpty() && numbers[stack.peek()] < numbers[i]) {
+                answer[stack.pop()] = numbers[i];
+            }
+
+            stack.push(i);
+        }
+
+        while(!stack.isEmpty()) {
+            answer[stack.pop()] = -1;
+        }
+
+        return answer;
+    }
+
+}
