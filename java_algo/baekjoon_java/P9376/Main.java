c066a828d150b3d8dd1f23c416dc1c799f442991
JJiWoNing <jjiwoning12@gmail.com>
2023-12-19 18:56:15 +0900

    boj 9376

--- java_algo/baekjoon_java/P9376/Main.java
+++ java_algo/baekjoon_java/P9376/Main.java
@@ -3,23 +3,25 @@ package baekjoon_java.P9376;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
-import java.util.HashSet;
-import java.util.Objects;
+import java.util.List;
 import java.util.PriorityQueue;
-import java.util.Set;
 import java.util.StringTokenizer;
 
 public class Main {
 
 	private static final int[] DX = new int[] {1, -1, 0, 0};
 	private static final int[] DY = new int[] {0, 0, 1, -1};
+	public static final int MAX = 987654321;
 
 	private static int n;
 	private static int m;
 	private static char[][] map;
-	private static boolean[][][][] visited; // doorCount, findPerson, x, y
+	private static int[][] outerVisited; // doorCount, x, y
+	private static int[][] prisoner1Visited; // doorCount, x, y
+	private static int[][] prisoner2Visited; // doorCount, x, y
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -33,41 +35,82 @@ public class Main {
 			m = Integer.parseInt(st.nextToken()) + 2;
 
 			map = new char[n][m];
-			int doorCount = 0;
 			Arrays.fill(map[0], '.');
 			Arrays.fill(map[n - 1], '.');
+
+			List<Node> prisoners = new ArrayList<>();
 
 			for (int i = 1; i < n - 1; i++) {
 				Arrays.fill(map[i], '.');
 				char[] chars = br.readLine().toCharArray();
 				for (int j = 1; j < m - 1; j++) {
 					map[i][j] = chars[j - 1];
-					if (chars[j - 1] == '#') {
-						doorCount++;
+					if (chars[j - 1] == '$') {
+						prisoners.add(new Node(i, j, 0));
 					}
 				}
 			}
 
-			visited = new boolean[doorCount + 1][3][n][m];
+			outerVisited = new int[n][m];
+			for (int i = 0; i < n; i++) {
+				Arrays.fill(outerVisited[i], MAX);
+			}
 
-			answer.append(bfs()).append("\n");
+			prisoner1Visited = new int[n][m];
+			for (int i = 0; i < n; i++) {
+				Arrays.fill(prisoner1Visited[i], MAX);
+			}
+
+			prisoner2Visited = new int[n][m];
+			for (int i = 0; i < n; i++) {
+				Arrays.fill(prisoner2Visited[i], MAX);
+			}
+
+			bfs(new Node(0, 0, 0), outerVisited); // 외부인
+			bfs(prisoners.get(0), prisoner1Visited); // 죄수 1
+			bfs(prisoners.get(1), prisoner2Visited); // 죄수 2
+
+			int[][] result = new int[n][m];
+
+			for (int i = 0; i < n; i++) {
+				for (int j = 0; j < m; j++) {
+					if (map[i][j] == '*') {
+						result[i][j] = Integer.MAX_VALUE;
+						continue;
+					}
+					if (outerVisited[i][j] == MAX || prisoner1Visited[i][j] == MAX || prisoner2Visited[i][j] == MAX) {
+						result[i][j] = Integer.MAX_VALUE;
+						continue;
+					}
+					result[i][j] += outerVisited[i][j] + prisoner1Visited[i][j] + prisoner2Visited[i][j];
+					if (map[i][j] == '#') {
+						result[i][j] -= 2;
+					}
+				}
+			}
+
+			int find = Integer.MAX_VALUE;
+
+			for (int[] ints : result) {
+				for (int num : ints) {
+					find = Math.min(find, num);
+				}
+			}
+
+			answer.append(find).append("\n");
 		}
 
 		System.out.println(answer);
 	}
 
-	private static int bfs() {
-		PriorityQueue<Node> priorityQueue = new PriorityQueue<>(Comparator.comparingInt(o -> o.visitedDoor.size()));
+	private static void bfs(Node start, int[][] visited) {
+		PriorityQueue<Node> priorityQueue = new PriorityQueue<>(Comparator.comparingInt(o -> o.doorCount));
 
-		priorityQueue.add(new Node(0, 0));
-		visited[0][0][0][0] = true;
+		priorityQueue.add(start);
+		visited[start.x][start.y] = 0;
 
 		while (!priorityQueue.isEmpty()) {
 			Node node = priorityQueue.poll();
-
-			if (node.findPerson.size() == 2) {
-				return node.visitedDoor.size();
-			}
 
 			for (int i = 0; i < 4; i++) {
 				int mx = node.x + DX[i];
@@ -77,7 +120,7 @@ public class Main {
 					continue;
 				}
 
-				if (visited[node.visitedDoor.size()][node.findPerson.size()][mx][my]) {
+				if (visited[mx][my] != MAX) {
 					continue;
 				}
 
@@ -86,91 +129,26 @@ public class Main {
 				}
 
 				if (map[mx][my] == '#') {
-					if (node.visitedDoor.contains(new Point(mx, my))) {
-						if (!visited[node.visitedDoor.size()][node.findPerson.size()][mx][my]) {
-							visited[node.visitedDoor.size()][node.findPerson.size()][mx][my] = true;
-							HashSet<Point> doors = new HashSet<>(node.visitedDoor);
-							doors.add(new Point(mx, my));
-							priorityQueue.add(new Node(mx, my, doors, new HashSet<>(node.findPerson)));
-						}
-					} else {
-						if (!visited[node.visitedDoor.size() + 1][node.findPerson.size()][mx][my]) {
-							visited[node.visitedDoor.size() + 1][node.findPerson.size()][mx][my] = true;
-							HashSet<Point> doors = new HashSet<>(node.visitedDoor);
-							doors.add(new Point(mx, my));
-							priorityQueue.add(new Node(mx, my, doors, new HashSet<>(node.findPerson)));
-						}
-					}
+					visited[mx][my] = node.doorCount + 1;
+					priorityQueue.add(new Node(mx, my, node.doorCount + 1));
+					continue;
 				}
 
-				if (map[mx][my] == '.') {
-					if (!visited[node.visitedDoor.size()][node.findPerson.size()][mx][my]) {
-						visited[node.visitedDoor.size()][node.findPerson.size()][mx][my] = true;
-						priorityQueue.add(new Node(mx, my, new HashSet<>(node.visitedDoor), new HashSet<>(node.findPerson)));
-					}
-				}
-
-				if (map[mx][my] == '$') {
-					if (node.findPerson.contains(new Point(mx, my))) {
-						continue;
-					}
-					visited[node.visitedDoor.size()][node.findPerson.size() + 1][mx][my] = true;
-					HashSet<Point> people = new HashSet<>(node.findPerson);
-					people.add(new Point(mx, my));
-					if (people.size() == 2) {
-						return node.visitedDoor.size();
-					}
-					priorityQueue.add(new Node(mx, my, node.visitedDoor, people));
-				}
+				visited[mx][my] = node.doorCount;
+				priorityQueue.add(new Node(mx, my, node.doorCount));
 			}
 		}
-
-		return 1;
 	}
 }
 
 class Node {
 	int x;
 	int y;
-	Set<Point> visitedDoor;
-	Set<Point> findPerson;
+	int doorCount;
 
-	public Node(int x, int y) {
+	public Node(int x, int y, int doorCount) {
 		this.x = x;
 		this.y = y;
-		this.visitedDoor = new HashSet<>();
-		this.findPerson = new HashSet<>();
-	}
-
-	public Node(int x, int y, Set<Point> visitedDoor, Set<Point> findPerson) {
-		this.x = x;
-		this.y = y;
-		this.visitedDoor = visitedDoor;
-		this.findPerson = findPerson;
-	}
-}
-
-class Point {
-	int x;
-	int y;
-
-	public Point(int x, int y) {
-		this.x = x;
-		this.y = y;
-	}
-
-	@Override
-	public boolean equals(Object o) {
-		if (this == o)
-			return true;
-		if (o == null || getClass() != o.getClass())
-			return false;
-		Point point = (Point)o;
-		return x == point.x && y == point.y;
-	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(x, y);
+		this.doorCount = doorCount;
 	}
 }
